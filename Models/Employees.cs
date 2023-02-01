4b0a6c3a6d170c5e43f610d59a8502c95b9abeda
James Cubbage <jscubbage@ucdavis.edu>
2023-02-01 14:43:40 -0800

    Getting ready to build

--- Models/Employees.cs
+++ Models/Employees.cs
@@ -7,5 +7,11 @@ namespace ServiceTracker.Models
         public string Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+
+        public string Name { 
+            get {
+                return FirstName + " " + LastName;
+            }
+        }
     }
 }
