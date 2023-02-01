d58d530431f818e99a24f400990212f8649a06b2
James Cubbage <jscubbage@ucdavis.edu>
2023-02-01 00:37:13 -0800

    clearing up warnings

--- Models/Committees.cs
+++ Models/Committees.cs
@@ -5,7 +5,7 @@ namespace ServiceTracker.Models
     public class Committees 
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Term { get; set; }
     }
 }
--- Models/Employees.cs
+++ Models/Employees.cs
@@ -5,7 +5,7 @@ namespace ServiceTracker.Models
     public class Employee
     {
         public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
