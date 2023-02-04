65deda552bfe769d2b0265893a337a065b1711b3
James Cubbage <jscubbage@ucdavis.edu>
2023-02-03 17:31:36 -0800

    edit and details updated

--- Controllers/CommitteeControler.cs
+++ Controllers/CommitteeControler.cs
@@ -23,16 +23,43 @@ namespace MvcMovie.Controllers
             return View(model);
         }
 
-        public async Task<IActionResult> Details(int id, int year)
+        public async Task<IActionResult> Details(int id, int ViewYear)
         {
-            var model = await CommitteeDetailsViewModel.Create(_context, id, year);
+            var model = await CommitteeDetailsViewModel.Create(_context, id, ViewYear);
             return View(model);
         }
 
         public async Task<IActionResult> Edit(int id)
         {           
             var model = await _context.Committees.Where(c => c.Id == id).FirstOrDefaultAsync();
+            if(model == null)
+            {
+                ErrorMessage = "Committee not found!";
+                return RedirectToAction(nameof(Index));
+            }
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Committees commitee)
+        {
+            var commiteeToUpdate = await _context.Committees.Where(c => c.Id == commitee.Id).FirstOrDefaultAsync();
+            if(commiteeToUpdate == null)
+            {
+                ErrorMessage = "Committee not found!";
+                return RedirectToAction(nameof(Index));
+            }
+            commiteeToUpdate.Name = commitee.Name;
+            commiteeToUpdate.Description = commitee.Description;
+            commiteeToUpdate.Term = commitee.Term;
+
+            if(ModelState.IsValid)
+            {
+                await _context.SaveChangesAsync();
+                Message = "Committee updated";
+                return RedirectToAction(nameof(Details), new { id = commitee.Id }); 
+            }
+            return View(commitee);
         }
 
        
--- Models/Committees.cs
+++ Models/Committees.cs
@@ -6,8 +6,10 @@ namespace ServiceTracker.Models
     public class Committees 
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Range(1,10)]
         public int Term { get; set; }
 
 
